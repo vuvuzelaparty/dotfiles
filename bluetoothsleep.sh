3fb339856788a9a36d8a790f31001b28673a21ca
Vuvuzelaparty <robhausler712@gmail.com>
2019-09-14 00:00:20 -0400

    edited bluetooth sleep script

--- bluetoothsleep.sh
+++ bluetoothsleep.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# Put this file in /lib/systemd/system-sleep
+# Put this file in /usr/lib/systemd/system-sleep
 # Follow the the instructions in .blu to apprpriately replace bluetooth_address
 # Don't forget to make this file executable!
 case $1/$2 in
-	pre/*) exit 0
+	pre/*) echo "power off" | bluetoothctl; exit 0
 		;;
 	post/*) echo "power on" | bluetoothctl; sleep 3s; echo "connect <bluetooth_address>" | bluetoothctl
 		;;
