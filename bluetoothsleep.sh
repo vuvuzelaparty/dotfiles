9ea506ca44a2943203b693a80c2b70f771945a83
Vuvuzelaparty <robhausler712@gmail.com>
2019-09-15 20:41:26 -0400

    mute speakers upon sleeping

--- bluetoothsleep.sh
+++ bluetoothsleep.sh
@@ -3,7 +3,7 @@
 # Follow the the instructions in .blu to apprpriately replace bluetooth_address
 # Don't forget to make this file executable!
 case $1/$2 in
-	pre/*) echo "power off" | bluetoothctl; exit 0
+	pre/*) echo "power off" | bluetoothctl; amixer set Master mute; exit 0
 		;;
 	post/*) echo "power on" | bluetoothctl; sleep 3s; echo "connect <bluetooth_address>" | bluetoothctl
 		;;
